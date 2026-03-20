c09573acd78906344baf55b8b6bb8cbea4393c34
Akash R <akshxdevs@gmail.com>
2026-03-20 19:16:33 +0530

    Handle bearer tokens safely and unify merchant and user authorization middleware error responses

--- server/dist/middleware.js
+++ server/dist/middleware.js
@@ -3,34 +3,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MerchantAuthMiddleware = exports.userAuthMiddleware = void 0;
+exports.merchantAuthMiddleware = exports.userAuthMiddleware = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const config_1 = require("./config");
 const userAuthMiddleware = (req, res, next) => {
-    const token = req.headers["authorization"];
-    const verified = jsonwebtoken_1.default.verify(token, config_1.USER_SECERT);
-    if (verified) {
+    try {
+        const header = req.headers["authorization"];
+        if (!header) {
+            return res.status(403).json({ message: "Token missing!" });
+        }
+        const token = header.startsWith("Bearer ") ? header.slice(7) : header;
+        const verified = jsonwebtoken_1.default.verify(token, config_1.USER_SECRET);
         req.ID = verified.ID;
         next();
     }
-    else {
-        return res.status(403).json({
-            message: "Not Authorzied!!"
-        });
+    catch (error) {
+        return res.status(403).json({ message: "Not Authorized!" });
     }
 };
 exports.userAuthMiddleware = userAuthMiddleware;
-const MerchantAuthMiddleware = (req, res, next) => {
-    const token = req.headers["authorization"];
-    const verified = jsonwebtoken_1.default.verify(token, config_1.MERCHANT_SECERT);
-    if (verified) {
+const merchantAuthMiddleware = (req, res, next) => {
+    try {
+        const header = req.headers["authorization"];
+        if (!header) {
+            return res.status(403).json({ message: "Token missing!" });
+        }
+        const token = header.startsWith("Bearer ") ? header.slice(7) : header;
+        const verified = jsonwebtoken_1.default.verify(token, config_1.MERCHANT_SECRET);
         req.ID = verified.ID;
         next();
     }
-    else {
-        return res.status(403).json({
-            message: "Not Authorzied!!"
-        });
+    catch (error) {
+        return res.status(403).json({ message: "Not Authorized!" });
     }
 };
-exports.MerchantAuthMiddleware = MerchantAuthMiddleware;
+exports.merchantAuthMiddleware = merchantAuthMiddleware;
--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -1,35 +1,37 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { MERCHANT_SECERT, USER_SECERT } from "./config";
+import { MERCHANT_SECRET, USER_SECRET } from "./config";
 
 export interface AuthRequest extends Request {
-    ID? : string;
+    ID?: string;
 }
 
-export const userAuthMiddleware = (req:AuthRequest,res:Response,next:NextFunction) =>{
-    const token = req.headers["authorization"] as unknown as string;
-    const verified:any = jwt.verify(token,USER_SECERT) as {id:string};
-
-    if (verified) {
-        req.ID = verified.ID
-        next()
-    }else{
-        return res.status(403).json({
-            message:"Not Authorzied!!"
-        })
+export const userAuthMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        const header = req.headers["authorization"];
+        if (!header) {
+            return res.status(403).json({ message: "Token missing!" });
+        }
+        const token = header.startsWith("Bearer ") ? header.slice(7) : header;
+        const verified: any = jwt.verify(token, USER_SECRET);
+        req.ID = verified.ID;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: "Not Authorized!" });
     }
-}
+};
 
-export const MerchantAuthMiddleware = (req:AuthRequest,res:Response,next:NextFunction) =>{
-    const token = req.headers["authorization"] as unknown as string;
-    const verified:any = jwt.verify(token,MERCHANT_SECERT) as {id:string};
-
-    if (verified) {
-        req.ID = verified.ID
-        next()
-    }else{
-        return res.status(403).json({
-            message:"Not Authorzied!!"
-        })
+export const merchantAuthMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        const header = req.headers["authorization"];
+        if (!header) {
+            return res.status(403).json({ message: "Token missing!" });
+        }
+        const token = header.startsWith("Bearer ") ? header.slice(7) : header;
+        const verified: any = jwt.verify(token, MERCHANT_SECRET);
+        req.ID = verified.ID;
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: "Not Authorized!" });
     }
-}
+};
